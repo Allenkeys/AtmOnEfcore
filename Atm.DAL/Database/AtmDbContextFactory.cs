a191d86791c2bb0958f8d21862b3abb4b8ad1cc0
Joshua Eze <ezejoshuac@gmail.com>
2023-02-28 15:02:00 +0100

    Add dbfactory to connect to database
    - Tested locally with localhost db

---
+++ Atm.DAL/Database/AtmDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Atm.DAL.Database
+{
+    internal class AtmDbContextFactory : IDesignTimeDbContextFactory<AtmDbContext>
+    {
+        public AtmDbContextFactory()
+        {
+
+        }
+        public AtmDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AtmDbContext>();
+            var connectionString = @"Data Source=DAGRAY\SQLEXPRESS;Integrated Security=True;Initial Catalog=AtmOnEfcore;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            optionsBuilder.UseSqlServer(connectionString);
+            Console.WriteLine(connectionString);
+            return new AtmDbContext(optionsBuilder.Options);
+        }
+    }
+}
